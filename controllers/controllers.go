5ab8b169df806a9719bd97e7b470ba39bed62123
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-02-21 00:02:12 -0300

    criado endpoint para criar novas personalidades

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -29,3 +29,10 @@ func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(personalidade)
 }
 
+func CriaUmaNovaPersonalidade(w http.ResponseWriter, r *http.Request){
+	var novaPersonalidade models.Personalidade
+	json.NewDecoder(r.Body).Decode(&novaPersonalidade)
+	database.DB.Create(&novaPersonalidade)
+	json.NewEncoder(w).Encode(novaPersonalidade)
+}
+
--- main.go
+++ main.go
@@ -4,17 +4,12 @@ import (
 	"fmt"
 
 	"github.com/jaquelineabreu/personalidades/database"
-	"github.com/jaquelineabreu/personalidades/models"
 	"github.com/jaquelineabreu/personalidades/routes"
 )
 
 
 
 func main() {
-	models.Personalidades = []models.Personalidade{
-		{Id: 1, Nome: "Nome1", Historia:"Historia 1"},
-		{Id: 2, Nome: "Nome2", Historia:"Historia 2"},
-	}
 
 	database.ConectaComBancoDeDados()
 
