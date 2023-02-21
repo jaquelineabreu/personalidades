f6b334364e416d243138e34dbe59ba5b8ed1b7a0
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-02-21 18:06:26 -0300

    instalada e configurada a lib handlers para que qualquer aplicação consiga acessar e consumir os dados da API

--- routes/routes.go
+++ routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/jaquelineabreu/personalidades/controllers"
 	"github.com/jaquelineabreu/personalidades/middleware"
@@ -18,6 +19,6 @@ func HandleRequest(){
 	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
 	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 
 }
